61a259c9dc5c9f0cc1e94365b9e31cab168dd7b5
Logosdibta <97156724+Logosdibta@users.noreply.github.com>
2023-09-03 18:25:03 +0800

    Delete Private Key hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["0x824f07c6d2d031f971c55035a4406b44b57ca69c4c528815c64ef5ee718f55c8"], //Your private key starting with "0x" 
+      accounts: ["0x.."], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
